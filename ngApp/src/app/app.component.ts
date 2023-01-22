ce923eedff8c8c0960f8ffeb1ef3b415c14d818b
yuzukatane <yuzu.merci@gmail.com>
2023-01-22 15:43:38 +0900

    use of html and css of app root

--- ngApp/src/app/app.component.ts
+++ ngApp/src/app/app.component.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <h1 class="text-center">
-      <span class="title">{{ title }}</span>
-      <p class="sub-title">{{ subtitle }}</p>
-    </h1>
-    <router-outlet></router-outlet>
-    `,
-  styles: [
-    '.title { color: #b0c4de;}',
-    '.sub-title { font-size: small; }'
-  ],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Simple Todo';
-  subtitle = 'Angular + Django Rest Framework + PostgreSql'
+	title = 'Simple Todo';
+	subtitle = 'Angular + Django Rest Framework + PostgreSql'
 }
--- ngApp/src/app/components/todo-list.component.ts
+++ ngApp/src/app/components/todo-list.component.ts
@@ -5,34 +5,34 @@ import { TodoService } from '../services/todo.service';
 import { Todo } from '../models/todo.model';
 
 @Component({
-  selector: 'todo-list',
-  templateUrl: '../templates/todo-list.component.html',
-  styleUrls: ['../static/todo-list.component.scss']
+	selector: 'todo-list',
+	templateUrl: '../templates/todo-list.component.html',
+	styleUrls: ['../static/todo-list.component.scss']
 })
 export class TodoListComponent {
-  todos: Todo[] = [];
-  @Input() todo: Todo = new Todo();
+	todos: Todo[] = [];
+	@Input() todo: Todo = new Todo();
 
-  constructor(
-    private todoService: TodoService,
-	private router: Router,
-  ){}
-  ngOnInit(): void {
-    this.todoService.getAllTodo()
-      .subscribe((todos: Todo[]) => this.todos = todos);
-  }
+	constructor(
+		private todoService: TodoService,
+	){}
+	
+	ngOnInit(): void {
+		this.todoService.getAllTodo()
+			.subscribe((todos: Todo[]) => this.todos = todos);
+	}
 
-  add(): void {
-	this.todoService
-		.add(this.todo)
-		.subscribe(todo => this.todos.push(this.todo));
-	window.location.reload();
-  }
+	add(): void {
+		this.todoService
+			.add(this.todo)
+			.subscribe(todo => this.todos.push(this.todo));
+		window.location.reload();
+	}
 
-  delete(id: number): void {
-    this.todoService
-      .delete(id)
-	  .subscribe();
-	  window.location.reload();
-  }
+	delete(id: number): void {
+		this.todoService
+			.delete(id)
+			.subscribe();
+			window.location.reload();
+	}
 }
